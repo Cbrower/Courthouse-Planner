b8d5cba30da4045d0fca211e20f87ac25b9060fd
Cole <cole.brower@gmail.com>
2021-11-16 10:01:29 -0800

    Add starter home page and table builder

---
+++ src/CourthousePlanner/buildTables.sql
@@ -0,0 +1,75 @@
+CREATE TABLE IF NOT EXISTS Address(
+	address_id INTEGER NOT NULL AUTO_INCREMENT,
+	street_num INTEGER,
+	street_name VARCHAR,
+	zip INTEGER,
+	city VARCHAR,
+	state VARCHAR,
+	PRIMARY KEY(address_id)
+);
+CREATE TABLE IF NOT EXISTS CourtRoom(
+	room_id INTEGER NOT NULL AUTO_INCREMENT,
+	address_id INTEGER NOT NULL,
+	district VARCHAR,
+	court_name VARCHAR,
+	room_num INTEGER,
+	PRIMARY KEY(room_id),
+	FOREIGN KEY(address_id) REFERENCES Address(address_id)
+);
+CREATE TABLE IF NOT EXISTS Person(
+	pid INTEGER NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	birth_date DATE,
+	phone_number CHAR(10), CHECKME
+	address_id INTEGER,
+	PRIMARY KEY(pid),
+	FOREIGN KEY(address_id) REFERENCES Address(address_id)
+);
+CREATE TABLE IF NOT EXISTS Defendant(
+	pid INTEGER NOT NULL,
+	PRIMARY KEY(pid)
+	FOREIGN KEY(pid) REFERENCES Person(pid)
+);
+CREATE TABLE IF NOT EXISTS Attorney(
+	pid INTEGER NOT NULL,
+	bar_id INTEGER NOT NULL,
+	PRIMARY KEY(pid)
+	FOREIGN KEY(pid) REFERENCES Person(pid)
+);
+CREATE TABLE IF NOT EXISTS Judge(
+	pid INTEGER NOT NULL,
+	PRIMARY KEY(pid)
+	FOREIGN KEY(pid) REFERENCES Person(pid)
+);
+CREATE TABLE IF NOT EXISTS CrimeDict(
+	penal_code INTEGER NOT NULL,
+	law_desc VARCHAR,
+	crime_type INTEGER,
+	PRIMARY KEY(penal_code)
+);
+CREATE TABLE IF NOT EXISTS CourtCase(
+	case_id INTEGER NOT NULL AUTO_INCREMENT,
+	case_div INTEGER NOT NULL,
+	outcome INTEGER NOT NULL,
+	def_id INTEGER NOT NULL,
+	def_att_id INTEGER NOT NULL,
+	pros_id INTEGER NOT NULL,
+	jid INTEGER NOT NULL,
+	crime_id INTEGER NOT NULL,
+	PRIMARY KEY(case_id),
+	FOREIGN KEY(def_id) REFERENCES Defendant(pid),
+	FOREIGN KEY(def_att_id) REFERENCES Attorney(pid),
+	FOREIGN KEY(pros_id) REFERENCES Attorney(pid),
+	FOREIGN KEY(jid) REFERENCES Judge(pid),
+	FOREIGN KEY(crime_id) REFERENCES CrimeDict(penal_code)
+	CHECK (def_att_id <> pros_id)
+);
+CREATE TABLE IF NOT EXISTS Appointment(
+	apt_id INTEGER NOT NULL AUTO_INCREMENT,
+	room_id INTEGER NOT NULL,
+	filing_fee REAL,
+	crime_code INTEGER,
+	PRIMARY KEY(apt_id),
+	FOREIGN KEY(room_id) REFERENCES CourtRoom(room_id)
+);
