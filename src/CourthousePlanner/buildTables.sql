acf4c841b78dbc34c17dd1e7b6b62c850ebac7e1
Cole <cole.brower@gmail.com>
2021-11-18 12:14:33 -0800

    Updated according to ER Diagram v3

--- src/CourthousePlanner/buildTables.sql
+++ src/CourthousePlanner/buildTables.sql
@@ -1,41 +1,19 @@
-CREATE TABLE IF NOT EXISTS Address(
-	address_id INTEGER NOT NULL AUTO_INCREMENT,
-	street_num INTEGER,
-	street_name VARCHAR,
-	zip INTEGER,
-	city VARCHAR,
-	state VARCHAR,
-	PRIMARY KEY(address_id)
-);
 CREATE TABLE IF NOT EXISTS CourtRoom(
 	room_id INTEGER NOT NULL AUTO_INCREMENT,
-	address_id INTEGER NOT NULL,
 	district VARCHAR,
 	court_name VARCHAR,
 	room_num INTEGER,
 	PRIMARY KEY(room_id),
-	FOREIGN KEY(address_id) REFERENCES Address(address_id)
 );
 CREATE TABLE IF NOT EXISTS Person(
 	pid INTEGER NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	birth_date DATE,
-	phone_number CHAR(10), CHECKME
-	address_id INTEGER,
+	phone_number CHAR(10),
+	is_lawyer BOOLEAN,
+	is_judge BOOLEAN,
 	PRIMARY KEY(pid),
-	FOREIGN KEY(address_id) REFERENCES Address(address_id)
-);
-CREATE TABLE IF NOT EXISTS Attorney(
-	pid INTEGER NOT NULL,
-	bar_id INTEGER NOT NULL,
-	PRIMARY KEY(pid)
-	FOREIGN KEY(pid) REFERENCES Person(pid)
-);
-CREATE TABLE IF NOT EXISTS Judge(
-	pid INTEGER NOT NULL,
-	PRIMARY KEY(pid)
-	FOREIGN KEY(pid) REFERENCES Person(pid)
 );
 CREATE TABLE IF NOT EXISTS CourtCase(
 	case_id INTEGER NOT NULL AUTO_INCREMENT,
@@ -46,9 +24,9 @@ CREATE TABLE IF NOT EXISTS CourtCase(
 	jid INTEGER NOT NULL,
 	PRIMARY KEY(case_id),
 	FOREIGN KEY(def_id) REFERENCES Person(pid),
-	FOREIGN KEY(def_att_id) REFERENCES Attorney(pid),
-	FOREIGN KEY(pros_id) REFERENCES Attorney(pid),
-	FOREIGN KEY(jid) REFERENCES Judge(pid),
+	FOREIGN KEY(def_att_id) REFERENCES Person(pid),
+	FOREIGN KEY(pros_id) REFERENCES Person(pid),
+	FOREIGN KEY(jid) REFERENCES Person(pid),
 	CHECK (def_att_id <> pros_id)
 );
 CREATE TABLE IF NOT EXISTS CrimeDict(
@@ -58,7 +36,7 @@ CREATE TABLE IF NOT EXISTS CrimeDict(
 	PRIMARY KEY(penal_code)
 );
 CREATE TABLE IF NOT EXISTS Charges
-	case_id INTEGER NOT NULL,
+	case_id INTEGER NOT NULL AUTO_INCREMENT,
 	penal_code INTEGER NOT NULL,
 	charged BOOLEAN,
 	PRIMARY KEY(case_id, penal_code),
