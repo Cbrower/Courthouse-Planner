a02f3a6d747eb791b423c3b3065880e3c74e06d7
devinstanley <93797681+devinstanley@users.noreply.github.com>
2021-11-22 17:13:02 -0800

    Added Forms for Each Table
    These are all just individual form PHP files that can be copied and pasted wherever they are needed.

---
+++ src/CourthousePlanner/Common/AppointmentForm.php
@@ -0,0 +1,22 @@
+<form action = form.php method = "post">
+    <div>
+        <label for = "Apt ID"> Apt ID: </label>
+        <input type = "number" name = "apt_id" required = "required" placeholder = "001-001" />
+    </div>
+    <div>
+        <label for = "Case ID"> Case ID: </label>
+        <input type = "number" name = "caseID" required = "required" placeholder = "001-001"/>
+    </div>
+    <div>
+        <label for = "Room ID"> Room ID: </label>
+        <input type = "number" name = "roomID" required = "required" placeholder = "0001"/>
+    </div>
+    <div>
+        <label for = "Filing Fee"> Filing Fee: </label>
+        <input type = "number" name = "filingFee" required = "required" placeholder = "$00.00"/>
+    </div>
+    <div>
+        <label for = "Penal Code"> Penal Code: </label>
+        <input type = "number" name = "penal_code" required = "required" placeholder = "01-001" />
+    </div>
+</form>
