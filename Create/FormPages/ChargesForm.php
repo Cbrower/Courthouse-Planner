bb06f6fda91c219433f34fb3c506a81b34d393bf
devinstanley <93797681+devinstanley@users.noreply.github.com>
2021-11-30 11:03:45 -0800

    Can Fully Register/Create Tuples
    - Can fully register for all tables as defined in this branch's buildTables.sql
    - Susceptible to MySQL injection, not sure if that is needed for this project
    - Still working on creating constraints

---
+++ Create/FormPages/ChargesForm.php
@@ -0,0 +1,37 @@
+<?php
+Include("../../Common/NavPanel.php");
+?>
+<head>
+    <div class="pagetitle">
+        <h1>Courtroom Planner</h1>
+    </div>
+    <link rel="stylesheet" href="../../Styles/Navbar.css">
+</head>
+<?php
+navPanel("../../");
+?>
+<head>
+    <link rel="stylesheet" type="text/css" href="../../Styles/FormStyle.css">
+</head>
+<form action = ../POSTForms/ChargesPost.php method = "post">
+    <div>
+        <label for = "Case ID"> Case ID: </label>
+        <input type = "number" name = "caseID" required = "required" placeholder = "001-001"/>
+    </div>
+    <div>
+        <label for = "Penal Code"> Penal Code: </label>
+        <input type = "number" name = "penal_code" required = "required" placeholder = "01-001" />
+    </div>
+    <div>
+        <label for = "Found"> Found: </label>
+        <p>
+            <input type="radio" id="guilty" name="outcome" value= TRUE>
+            <label for="guilty">Guilty</label><br>
+            <input type="radio" id="not_guilty" name="outcome" value= FALSE>
+            <label for="not_guilty">Not Guilty</label><br>
+        </p>
+    </div>
+    <div>
+        <button name = "submit" value = "submit" type="submit">Register</button>
+    </div>
+</form>
