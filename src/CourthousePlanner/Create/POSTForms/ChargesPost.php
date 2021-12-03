a1a120af0372c71854ea0f9697cd5929edeae7e5
devinstanley <93797681+devinstanley@users.noreply.github.com>
2021-12-03 12:01:13 -0800

    Added In Progress Option For Charges

--- src/CourthousePlanner/Create/POSTForms/ChargesPost.php
+++ src/CourthousePlanner/Create/POSTForms/ChargesPost.php
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     $case_ID = $_POST['caseID'];
     $penal_code = $_POST['penal_code'];
     $charged = $_POST['outcome'];
-    $sql = "INSERT INTO Charges (case_id,penal_code,charged) VALUES ('$case_ID','$penal_code','$charged')";
+    $sql = "INSERT INTO Charges (case_id,penal_code,charged) VALUES ('$case_ID','$penal_code', $charged)";
     if (mysqli_query($conn, $sql)) {
         echo '<div style="font-size:1.25em;">New record has been added successfully !</div>';
     } else {
