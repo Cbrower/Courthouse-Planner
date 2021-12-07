14ebf103f784125548178aa2cb1ea9f5fe48f180
Cole <cole.brower@gmail.com>
2021-12-06 19:03:05 -0800

    First draft of ensuring dates do not overlap

--- src/CourthousePlanner/Create/POSTForms/AppointmentPost.php
+++ src/CourthousePlanner/Create/POSTForms/AppointmentPost.php
@@ -24,13 +24,27 @@ if(isset($_POST['submit']))
     $penal_code = $_POST['penal_code'];
     $date = $_POST['date'];
 
-    $sql = "INSERT INTO Appointment (case_id,room_id,filing_fee,crime_code, apt_date) VALUES ('$case_ID','$room_ID','$filing_fee','$penal_code','$date')";
-    if ($conn->query($sql)) {
-        echo '<div style="font-size:1.25em;">New record has been added successfully !</div>';
+    $query = "SELECT * FROM Appointment WHERE room_id = " . $room_ID . " AND apt_date = " . $date;
+    $res = $conn->query($query);
+
+    $flag = true;
+    for ($res as $row) {
+	$flag = false;
+	break;
+    }
+
+    if ($flag) {
+        echo "<div style=\"font-size:1.25em;\">Error: Appointment already booked on date: " . date("F jS, Y", strtotime($date)) . "</div>";
     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+        $sql = "INSERT INTO Appointment (case_id,room_id,filing_fee,crime_code, apt_date) VALUES ('$case_ID','$room_ID','$filing_fee','$penal_code','$date')";
+        if ($conn->query($sql)) {
+            echo '<div style="font-size:1.25em;">New record has been added successfully !</div>';
+        } else {
+            echo "Error: " . $sql . ":-" . mysqli_error($conn);
+        }
     }
     mysqli_close($conn);
+
 }
 else{
     echo '<div style="font-size:1.25em;">Error During Submission. Please Try Again.</div>';
